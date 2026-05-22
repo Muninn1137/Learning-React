183722ee6642dcfa215d56a8a25eabcf0949d283
Muninn1137 <152490966+Muninn1137@users.noreply.github.com>
2026-05-22 17:08:53 +0200

    checkpoint

--- React/es1/index.jsx
+++ React/es1/index.jsx
@@ -1,7 +1,45 @@
 import {createRoot} from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
-root.render(
+import "./style.css"
 
-    <h1>Muninn is great</h1>
+root.render(
+    <>
+        <Header />
+        <Title />
+        <Mylist />
+        <Footer/>
+    </>
 )
+
+
+function Header(){
+    return(
+        <img src="React logo.png" width="40px" alt="react logo"></img>
+    )
+}
+
+function Title(){
+    return(
+      <h1>What does Muninn like?</h1>
+    )
+
+}
+
+function Footer(){
+    return(
+        <p>@ 2026 Copyright Muninn development. All rights reserved.</p>
+    )
+}
+
+function Mylist(){
+ return (
+  
+        <ol>
+            <li>Volleyball</li>
+            <li>Gaming </li>
+            <li>Karate</li>
+            <li>Goth girls</li>
+        </ol>   
+ )
+}
